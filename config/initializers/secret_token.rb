2d92d2e0cf1fd0b83e36e05448217bed18f53f13
William <lliw1236@yahoo.com>
2012-04-05 19:19:22 -0700

    Intial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RandomizableSlotImage::Application.config.secret_token = '5e8d4923289b005851367ec4cbbc71a7f5198d781b9d324426459364688babd4a8aee94e6ed9f907a0be692832358cadf60e561d4497e1df21219f35803162aa'
